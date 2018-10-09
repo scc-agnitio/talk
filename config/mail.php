4b7d8fcc2cd78607ac49b9fc1dab8ba2484bbf91
Sam <sam.ciaramilaro@gmail.com>
2018-10-09 22:24:31 +0200

    Add passport

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'tech-talks@charmwebservices.com'),
+        'name' => env('MAIL_FROM_NAME', 'Tech Talks'),
     ],
 
     /*
